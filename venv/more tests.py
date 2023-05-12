8b88ff1e4fd5e0c7774c3f123ff06f48d37b2ff7
Seppuku <Iamemperor@yandex.ru>
2023-05-12 13:50:16 +0300

    Задачка гитхаб https://www.codewars.com/kata/55b3425df71c1201a800009c/train/python 24 теста пройдено, но не пропустили (Скрин в тг выслал с ошибками)

---
+++ venv/more tests.py
@@ -0,0 +1,20 @@
+from statistics import median
+
+a = "01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"
+b = [[int(j) for j in i.split('|')] for i in a.split(', ')]
+list_secs = [i[0] * 3600 + i[1] * 60 + i[2] for i in b]
+range = max(list_secs) - min(list_secs)
+avg = sum(list_secs) / len(list_secs)
+med_ = median(list_secs)
+list_times = [range, avg, med_]
+
+def convert_to_time(sec):
+    sec = sec % (24 * 3600)
+    hour = sec // 3600
+    sec %= 3600
+    min = sec // 60
+    sec %= 60
+    return "%02d|%02d|%02d" % (hour, min, sec)
+
+time_format = list(map(convert_to_time, list_times))
+print(f'Range: {time_format[0]} Average: {time_format[1]} Median: {time_format[2]}')
