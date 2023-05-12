6374d0fee072b2da70b61353dc92104ebfe8841a
Seppuku <Iamemperor@yandex.ru>
2023-05-12 13:47:10 +0300

    Задачка гитхаб https://www.codewars.com/kata/55b3425df71c1201a800009c/train/python 24 теста пройдено, но не пропустили (Скрин в тг выслал с ошибками)

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-word = "Данное предложение будет разбиваться на отдельные слова"
+import string
 
-# Подсчитываем количество вхождений каждой буквы в слове
-c = dict()
-for letter in word:
-    c[letter] = c.get(letter, 0) + 1
-    # .get(letter, 0) вернет значение по ключу letter или 0, если такого ключа нет
-print(c)  # {'п': 1, 'р': 2, 'и': 2, 'о': 1, 'т': 2, 'е': 1}
-# Выводим ключ, которому соответствует наибольшее из значений
-# (точнее, один из таких ключей)
 
+class Alphabet:
+
+    def info(self):
+        print(f"{self.lang}")
+        print(f"{self.letters}")
+
+    def __init__(self, lang, letters):
+        self.lang = lang
+        self.letters = list(letters)
+
+    def print(self):
+        print(f"Буквы алфавита: {self.letters}")
+
+    def letters_num(self):
+        print(f"{len(self.letters)}")
+
+
+class EngAlphabet(Alphabet):
+    __letters_num = 26
+    default_lang = "En"
+
+    def __init__(self):
+        super().__init__(EngAlphabet.default_lang, string.ascii_uppercase)
+
+    def is_en_letter(self, letter):
+        if letter in self.letters:
+            return True
+        return False
+
+    def letters_num(self):
+        return EngAlphabet.__letters_num
+
+    @staticmethod
+    def example():
+        print(f"Пример текста на английском языке:\nDont judje book by its cover")
+
+
+if __name__ == '__main__':
+    eng_alphabet = EngAlphabet()
+    eng_alphabet.print()
+    print(eng_alphabet.letters_num())
+    print(eng_alphabet.is_en_letter('F'))
+    print(eng_alphabet.is_en_letter('Щ'))
+    EngAlphabet.example()
--- tests.py
+++ tests.py
@@ -1,68 +1,17 @@
-"""Готово......."""
-
-NUMBERS_ = list(range(1, 10))
-
-c = input("Приветствую Вас в игре крестики-нолики!\n"
-          "Правила Вам известны.\n"
-          "Для того, чтобы начать играть, введите сюда что-нибудь: ")
-
-
-def game_board(numbers_: list):
-    print("-" * 13)
-    for i in range(3):
-        print("|", numbers_[0 + i * 3], "|", numbers_[1 + i * 3], "|", numbers_[2 + i * 3], "|")
-        print("-" * 13)
-
-
-game_board(NUMBERS_)
+guess = 'world'
+solution = 'bleed'
+answer = []
+for k, v in zip(guess, solution):
+    if k in solution and k == v:
+        k = 'g'
+        answer.append(k)
+    elif k not in solution:# or guess_.count(k) > solution.count(v):
+        k = 'b'
+        answer.append(k)
+    elif k in solution and guess.index(k) != solution.index(v) or guess.count(k) > solution.count(v):
+        k = 'y'
+        answer.append(k)
 
 
-def win_condition(numbers_: list) -> bool:
-    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-    win = False
-    for i in win_coord:
-        if numbers_[i[0]] == numbers_[i[1]] == numbers_[i[2]]:
-            print(f"Поздравляю с победой, игрок {numbers_[i[0]]}!!!!")
-            win = True
-            break
-        else:
-            win = False
-
-    return win
-
-
-while True:
-
-    a = int(input("Ходит игрок X, введите номер клетки от 1 до 9: "))
-
-    if a > 9 or a <= 0:
-        print("Ошибочный ввод, повторите попытку")
-        continue
-
-    if str(NUMBERS_[a - 1]) in 'XO':
-        print("Клетка уже занята, выберите другую")
-        continue
-
-    if a in NUMBERS_:
-        NUMBERS_[a - 1] = "X"
-        game_board(NUMBERS_)
-
-    if win_condition(NUMBERS_):
-        break
-
-    b = int(input("Ходит игрок O, введите номер клетки от 1 до 9: "))
-
-    if b > 9 or b <= 0:
-        print("Ошибочный ввод, повторите попытку ")
-        b = int(input("Ходит игрок O, введите номер клетки от 1 до 9: "))
-
-    if str(NUMBERS_[b - 1]) in 'XO':
-        print("Клетка уже занята, выберите другую")
-        b = int(input("Ходит игрок O, введите номер клетки от 1 до 9: "))
-
-    if b in NUMBERS_:
-        NUMBERS_[b - 1] = "O"
-        game_board(NUMBERS_)
-
-    if win_condition(NUMBERS_):
-        break
+print(''.join(answer))
+print('correct answer: byggg')
